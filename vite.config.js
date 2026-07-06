f41b410faae1c428a28a1233ca21f3958dba4ea9
MatthewMartin09 <johnmatthewmartin09@gmail.com>
2026-07-06 19:43:44 +0800

    update base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This base path is critical for GitHub Pages project sites.
+  // It ensures your CSS and JS files are found at 
+  // https://matthewmartin09.github.io/flowers/
+  base: '/flowers/', 
 })
